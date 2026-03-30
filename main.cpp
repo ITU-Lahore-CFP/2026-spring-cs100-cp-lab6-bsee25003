c30ba2bd7accc261c1cba648a7dcca400b07241a
bsee25003 <bsee25003@itu.edu.pk>
2026-03-30 10:50:10 +0500

    BSEE-25003

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 
 
 int main() {
-   
+    int n;  // Variable to store user input
+
+    // Ask user to enter a number
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // Display sum of squares from 1 to n
+    cout << "Sum of squares: " << sumOfSquares(n);
+
+    // Display factorial of the number
+    cout << "\nFactorial: " << findFactorial(n);
+
+    return 0;  // Indicate successful program execution
 }
